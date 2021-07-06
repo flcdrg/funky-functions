bc7b436aa3e74ff3b5e93776a817d285a44bb93f
David Gardiner <david@gardiner.net.au>
2021-07-06 20:30:36 +0930

    Configure service bus trigger

--- src/net31/ServiceBusQueueTrigger.cs
+++ src/net31/ServiceBusQueueTrigger.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,9 +9,9 @@ namespace net31
     public static class ServiceBusQueueTrigger
     {
         [FunctionName("ServiceBusQueueTrigger")]
-        public static void Run([ServiceBusTrigger("myqueue", Connection = "")]string myQueueItem, ILogger log)
+        public static void Run([ServiceBusTrigger("cumber", Connection = "ServiceBusConnection")] Message message, ILogger log)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {message.MessageId}");
         }
     }
 }
