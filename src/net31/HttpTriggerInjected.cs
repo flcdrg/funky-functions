8c89221dbe3999ecd4f34e8f4718342df2b4b433
David Gardiner <david@gardiner.net.au>
2021-07-11 17:47:54 +0930

    Add HttpTriggerInjected to net31

---
+++ src/net31/HttpTriggerInjected.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace net31
+{
+    public class HttpTriggerInjected
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<HttpTriggerInjected> _log;
+
+        public HttpTriggerInjected(HttpClient httpClient, ILogger<HttpTriggerInjected> log)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _log = log ?? throw new ArgumentNullException(nameof(log));
+        }
+
+        [FunctionName("HttpTriggerInjected")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
+        {
+            _log.LogInformation("Running HttpTriggerInjected");
+
+            var response = await _httpClient.GetAsync("https://david.gardiner.net.au");
+
+            return new OkObjectResult($"Got request {req.Path} and response {response.StatusCode}");
+        }
+    }
+}
---
+++ src/net31/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace net31
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+        }
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            base.ConfigureAppConfiguration(builder);
+        }
+    }
+}
